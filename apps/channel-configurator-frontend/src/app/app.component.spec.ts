fcb93e41883a6e19f518646874b850c086f62899
amorscher <am.morscher@gmail.com>
2023-11-26 12:46:36 +0100

    fix test deps

--- apps/channel-configurator-frontend/src/app/app.component.spec.ts
+++ apps/channel-configurator-frontend/src/app/app.component.spec.ts
@@ -2,11 +2,23 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ChannelsFeatureChannelListModule } from '@channels/feature-channel-list';
+import { StoreModule } from '@ngrx/store';
+import { ChannelsDomainModule } from '@channels/domain';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        ChannelsFeatureChannelListModule,
+        EffectsModule.forRoot([]),
+        StoreModule.forRoot(),
+        ChannelsDomainModule,
+      ],
       declarations: [AppComponent, NxWelcomeComponent],
     }).compileComponents();
   });
@@ -16,7 +28,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome channels'
+      'channels-channel-list'
     );
   });
 
