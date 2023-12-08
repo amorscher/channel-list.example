11ef695782e376a9a75768ba229998e208cb4045
amorscher <am.morscher@gmail.com>
2023-12-08 14:31:52 +0100

    fix compilation and test errors

--- apps/channel-configurator-frontend/src/test-setup.ts
+++ apps/channel-configurator-frontend/src/test-setup.ts
@@ -5,4 +5,28 @@ globalThis.ngJest = {
         errorOnUnknownProperties: true,
     },
 };
+import { SyncAction } from '@channels/domain-entities';
 import 'jest-preset-angular/setup-jest';
+
+/**
+ * Mocks the client sockets: just keeps track of all registered listeners
+ */
+class ClientSocketMock {
+    public listeners: ((payload: SyncAction<unknown, unknown>) => void)[] = [];
+
+    on(_type: string, listener: () => void) {
+        this.listeners.push(listener);
+    }
+}
+
+const mSocket = new ClientSocketMock();
+
+// mock socket io client
+jest.mock('socket.io-client', () => {
+    return { io: jest.fn(() => mSocket) };
+});
+
+// mock socket io client
+jest.mock('socket.io-client', () => {
+    return { io: jest.fn(() => mSocket) };
+});
--- libs/shared/ui-base-components/src/index.ts
+++ libs/shared/ui-base-components/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/shared-ui-base-components.module';
+export * from './lib/main-layout/main-layout.component';
+export * from './lib/theme-picker/theme-picker.component';
